00acd4fe895d6d5292cd97afc9f99b43be702b94
Red_Spash <red.spash.game@gmail.com>
2024-02-07 23:02:02 +0100

    Convert role interface to abstract class

--- src/fr/red_spash/murder/game/roles/Role.java
+++ src/fr/red_spash/murder/game/roles/Role.java
@@ -1,13 +1,35 @@
 package fr.red_spash.murder.game.roles;
 
+import fr.red_spash.murder.players.PlayerManager;
+import fr.red_spash.murder.utils.ItemStackBuilder;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-public interface Role {
-    String getRoleColor();
-    String getName();
-    String shortDescription();
-    String getDescription();
-    void giveItems(Player p);
-    Sound getSound();
+public abstract class Role {
+
+    public abstract String getRoleColor();
+    public abstract String getName();
+    public abstract String shortDescription();
+    public abstract String getDescription();
+    public abstract Sound getSound();
+    public void giveItems(Player p){
+
+    }
+
+    public void giveBow(Player p){
+        ItemStack itemStack = p.getInventory().getItem(Detective.SLOT_BOW);
+        p.getInventory().setItem(Detective.SLOT_BOW, Detective.DETECTIVE_BOW);
+        if(itemStack == null){
+            p.getInventory().setItem(Detective.SLOT_ARROW, new ItemStack(Material.ARROW));
+        }else{
+            p.getInventory().getItem(Detective.SLOT_ARROW).setAmount(
+                    p.getInventory().getItem(Detective.SLOT_ARROW).getAmount()+1
+            );
+        }
+    }
+    public boolean isMurder(){
+        return false;
+    }
 }
