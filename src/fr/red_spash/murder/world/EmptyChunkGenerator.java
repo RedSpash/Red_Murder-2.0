4a2696fd828b84b55813bc05c3502a3d6de1408d
Red_Spash <red.spash.game@gmail.com>
2024-03-03 22:39:22 +0100

    Added void chunk generator

---
+++ src/fr/red_spash/murder/world/EmptyChunkGenerator.java
@@ -0,0 +1,50 @@
+package fr.red_spash.murder.world;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
+
+public class EmptyChunkGenerator extends ChunkGenerator {
+
+    @Override
+    public boolean shouldGenerateNoise() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateSurface() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateBedrock() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateCaves() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateDecorations() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateMobs() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldGenerateStructures() {
+        return false;
+    }
+
+    @Override
+    public Location getFixedSpawnLocation(World world, Random random) {
+        return new Location(world, 0, 250, 0);
+    }
+}
