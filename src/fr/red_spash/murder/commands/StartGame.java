97c24f28b0541f3f847f76bdc55b00d87b132af1
Red_Spash <red.spash.game@gmail.com>
2024-03-03 22:40:03 +0100

    Added game timers

--- src/fr/red_spash/murder/commands/StartGame.java
+++ src/fr/red_spash/murder/commands/StartGame.java
@@ -2,11 +2,17 @@ package fr.red_spash.murder.commands;
 
 import fr.red_spash.murder.game.GameManager;
 import fr.red_spash.murder.maps.GameMap;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class StartGame implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartGame implements CommandExecutor, TabCompleter {
 
     private GameManager gameManager;
 
@@ -17,18 +23,30 @@ public class StartGame implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender.isOp()){
+        if(commandSender.isOp() && strings.length == 1){
             GameMap findedGameMap = null;
             for(GameMap gameMap : this.gameManager.getMapManager().getMaps()){
-                if(gameMap.getName().equalsIgnoreCase("amarante")){
+                if(gameMap.getName().equalsIgnoreCase(strings[0])){
                     findedGameMap = gameMap;
                     break;
                 }
             }
             if(findedGameMap != null){
                 this.gameManager.startPreGame(findedGameMap);
+            }else{
+                commandSender.sendMessage("§cCarte introuvable!");
             }
         }
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        ArrayList<String> completer = new ArrayList<>();
+        for(GameMap gameMap : this.gameManager.getMapManager().getMaps()){
+            completer.add(gameMap.getName());
+        }
+
+        return completer;
     }
 }
--- src/fr/red_spash/murder/game/tasks/GameTimerTask.java
+++ src/fr/red_spash/murder/game/tasks/GameTimerTask.java
@@ -1,56 +1,95 @@
 package fr.red_spash.murder.game.tasks;
 
 import fr.red_spash.murder.game.GameManager;
-import fr.red_spash.murder.game.roles.Role;
+import fr.red_spash.murder.game.roles.*;
+import fr.red_spash.murder.game.roles.concrete_roles.Detective;
+import fr.red_spash.murder.game.roles.concrete_roles.Innocent;
+import fr.red_spash.murder.game.roles.concrete_roles.Murder;
+import fr.red_spash.murder.game.roles.concrete_roles.Schizophrenic;
+import fr.red_spash.murder.game.scoreboard.ScoreboardLines;
 import fr.red_spash.murder.players.PlayerData;
 import fr.red_spash.murder.players.PlayerManager;
+import fr.red_spash.murder.utils.Utils;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitTask;
 
 public class GameTimerTask implements Runnable{
 
+    private static final int SCHIZOPHRENIC_TIMER = 2;//60*1;
     private final GameManager gameManager;
-    public static final int MAX_TIME = 60*5;
+    public static final int MAX_TIME = 60*7;
     private final PlayerManager playerManager;
     private final BukkitTask bukkitTask;
-    private int time = -1;
+    private final ScoreboardLines scoreboardLines;
+    private int time = 0;
 
-    public GameTimerTask(GameManager gameManager, Plugin main) {
+    public GameTimerTask(GameManager gameManager, Plugin main, ScoreboardLines scoreboardLines) {
         this.gameManager = gameManager;
         this.playerManager = gameManager.getPlayerManager();
+        this.scoreboardLines = scoreboardLines;
+
         this.bukkitTask = Bukkit.getScheduler().runTaskTimer(main, this, 20,20);
     }
 
     @Override
     public void run() {
-        this.time = this.time + 1;
-
-        if(this.time == 5){
-            for(Player p : Bukkit.getOnlinePlayers()){
+        if(this.time == 5) {
+            for (Player p : Bukkit.getOnlinePlayers()) {
                 PlayerData playerData = this.playerManager.getData(p);
-                Role role = playerData.getRole();
+                Role role = playerData.getVisualRole();
 
-                p.sendTitle(role.getRoleColor()+"ROLE: §l"+role.getName().toUpperCase(),"§e"+role.shortDescription(),10,20*5,10);
-                p.sendMessage("\n"+role.getRoleColor()+"§l"+role.getName().toUpperCase()+"\n§7"+role.getDescription()+"\n§f");
-                p.playSound(p.getLocation(), role.getSound(), 5,1);
-
+                role.setDiscovered(true);
+                p.sendTitle(role.getMinecraftRoleColor() + "ROLE: §l" + role.getName().toUpperCase(), "§e" + role.shortDescription(), 10, 20 * 5, 10);
+                p.sendMessage("\n" + role.getMinecraftRoleColor() + "§l" + role.getName().toUpperCase() + "\n§7" + role.getDescription() + "\n§f");
+                p.playSound(p.getLocation(), role.getSound(), 5, 1);
                 role.giveItems(p);
+                this.scoreboardLines.updateShow(p);
             }
+        } else if (this.time == SCHIZOPHRENIC_TIMER) {
+            for(PlayerData playerData : this.playerManager.getAllPlayerData()){
+                if(playerData.getTrueRole() != null &&
+                        (playerData.getTrueRole() instanceof Schizophrenic schizophrenic) && !playerData.isSpectator()){
+
+                    Player p = Bukkit.getPlayer(playerData.getUUID());
+                    if(p!= null && p.isOnline()){
+                        Role role = new Murder();
+                        if(Utils.generateRandomNumber(0,1) == 0){
+                            role = new Detective();
+                        }
+
+                        schizophrenic.setSubRole(role);
+                        role.giveItems(p);
+                        p.sendTitle(role.getMinecraftRoleColor()+role.getName(),role.shortDescription(),10,20*3,10);
+                        Bukkit.broadcastMessage(role.getMinecraftRoleColor()+"Le "+schizophrenic.getName()+" vient de passer "+schizophrenic.getSubRole().getName()+"!");
+                        for(Player pl: Bukkit.getOnlinePlayers()){
+                            if(role instanceof Murder){
+                                pl.playSound(pl.getLocation(), Sound.AMBIENT_CAVE,2,0);
+                                pl.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS,3*20,2,false,false,false));
+                            }
+                        }
+                    }
+                }
+            }
+            this.gameManager.checkEnd();
         }else if(this.time >= MAX_TIME){
-            this.gameManager.stopGame();
-            this.bukkitTask.cancel();
-            return;
+                this.gameManager.stopGame(new Innocent());
+                this.bukkitTask.cancel();
+                return;
+            }
+            this.time = this.time + 1;
         }
-    }
 
     public int getMaxTime() {
         return MAX_TIME;
     }
 
     public int getRemainingSeconds(){
-        return this.MAX_TIME - time;
+        return MAX_TIME - time;
     }
 
     public int getTime() {
