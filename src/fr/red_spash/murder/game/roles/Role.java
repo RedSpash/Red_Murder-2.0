823c8e13e2f0dfdbef8f65863ebe7ecbc4302ab6
Red_Spash <red.spash.game@gmail.com>
2024-03-03 22:39:37 +0100

    Edited giveBow method for detectives

--- src/fr/red_spash/murder/game/roles/Role.java
+++ src/fr/red_spash/murder/game/roles/Role.java
@@ -1,13 +1,20 @@
 package fr.red_spash.murder.game.roles;
 
+import fr.red_spash.murder.game.roles.concrete_roles.Detective;
+import fr.red_spash.murder.utils.ItemStackBuilder;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.awt.*;
+
 public abstract class Role {
 
-    public abstract String getRoleColor();
+    private boolean discovered = false;
+
+    public abstract String getMinecraftRoleColor();
+    public abstract Color getRoleColor();
     public abstract String getName();
     public abstract String shortDescription();
     public abstract String getDescription();
@@ -17,17 +24,28 @@ public abstract class Role {
     }
 
     public void giveBow(Player p){
-        ItemStack itemStack = p.getInventory().getItem(Detective.SLOT_BOW);
-        p.getInventory().setItem(Detective.SLOT_BOW, Detective.DETECTIVE_BOW);
+        ItemStack itemStack = p.getInventory().getItem(Detective.SLOT_ARROW);
+
+        if(p.getInventory().getItem(Detective.SLOT_BOW) == null){
+            p.getInventory().setItem(Detective.SLOT_BOW, new ItemStackBuilder(Material.BOW).setUnbreakable(true).setName("§aArc").hideAttributes().toItemStack());
+        }
         if(itemStack == null){
             p.getInventory().setItem(Detective.SLOT_ARROW, new ItemStack(Material.ARROW));
         }else{
-            p.getInventory().getItem(Detective.SLOT_ARROW).setAmount(
-                    p.getInventory().getItem(Detective.SLOT_ARROW).getAmount()+1
+            itemStack.setAmount(
+                    itemStack.getAmount()+1
             );
         }
     }
     public boolean isMurder(){
         return false;
+    }
+
+    public void setDiscovered(boolean discovered) {
+        this.discovered = discovered;
+    }
+
+    public boolean isDiscovered() {
+        return this.discovered;
     }
 }
