f4498941cda026f09fa98fe21bb19480465f7be8
Red_Spash <red.spash.game@gmail.com>
2024-03-03 22:34:20 +0100

    Removed bow pickup from spectator

--- src/fr/red_spash/murder/event/BowOnGroundListener.java
+++ src/fr/red_spash/murder/event/BowOnGroundListener.java
@@ -1,14 +1,16 @@
 package fr.red_spash.murder.event;
 
 import fr.red_spash.murder.Murder;
-import fr.red_spash.murder.game.roles.Detective;
-import fr.red_spash.murder.game.roles.Innocent;
+import fr.red_spash.murder.game.roles.concrete_roles.Detective;
+import fr.red_spash.murder.game.roles.concrete_roles.Innocent;
 import fr.red_spash.murder.game.roles.Role;
 import fr.red_spash.murder.game.tasks.BowParticleAndRotation;
 import fr.red_spash.murder.players.PlayerData;
 import fr.red_spash.murder.players.PlayerManager;
+import fr.red_spash.murder.utils.ItemStackBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -17,6 +19,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.EulerAngle;
 
 import java.util.HashMap;
@@ -24,6 +27,11 @@ import java.util.Map;
 
 public class BowOnGroundListener implements Listener {
 
+    private static final int BOW_COMPASS_SLOT = 2;
+    private static final ItemStack BOW_COMPASS = new ItemStackBuilder(Material.COMPASS)
+            .setName("§6Position de l'arc")
+            .setLore("§7Indique la position","§7de l'arc !")
+            .toItemStack();
     private final HashMap<Location, ArmorStand> bowsLocation = new HashMap<>();
     private final PlayerManager playerManager;
 
@@ -34,7 +42,7 @@ public class BowOnGroundListener implements Listener {
 
     @EventHandler
     public void playerMoveEvent(PlayerMoveEvent e){
-        if(bowsLocation.isEmpty())return;
+        if(this.bowsLocation.isEmpty())return;
 
         Player p = e.getPlayer();
         for (Map.Entry<Location,ArmorStand> entry : bowsLocation.entrySet()) {
@@ -42,8 +50,8 @@ public class BowOnGroundListener implements Listener {
             ArmorStand armorStand = entry.getValue();
             if(location.distance(p.getLocation()) <= 1.0){
                 PlayerData playerData = this.playerManager.getData(p);
-                Role role = playerData.getRole();
-                if(role instanceof Innocent innocent){
+                Role role = playerData.getVisualRole();
+                if(role instanceof Innocent innocent && !playerData.isSpectator()){
                     armorStand.remove();
                     this.bowsLocation.remove(location);
                     Bukkit.broadcastMessage("§aL'arc du détective vient d'être ramassé!");
@@ -51,6 +59,9 @@ public class BowOnGroundListener implements Listener {
                     innocent.giveBow(p);
                     p.sendMessage("§aVous recevez l'arc du détective!");
                     p.playSound(p.getLocation(), Sound.BLOCK_BUBBLE_COLUMN_UPWARDS_INSIDE,1,2);
+                    for(Player player : Bukkit.getOnlinePlayers()){
+                        player.getInventory().setItem(BOW_COMPASS_SLOT,null);
+                    }
                 }
             }
         }
@@ -73,6 +84,16 @@ public class BowOnGroundListener implements Listener {
         armorStand.setMarker(true);
         armorStand.getEquipment().setItemInMainHand(Detective.DETECTIVE_BOW);
         armorStand.teleport(location);
+
+        Bukkit.broadcastMessage("§a§lL'arc est au sol !");
+        for(Player p : Bukkit.getOnlinePlayers()){
+            p.playSound(p.getLocation(), Sound.ITEM_BUNDLE_DROP_CONTENTS,1,1);
+            PlayerData playerData = this.playerManager.getData(p.getUniqueId());
+            if(!playerData.isSpectator()){
+                p.getInventory().setItem(BOW_COMPASS_SLOT, BOW_COMPASS);
+                p.setCompassTarget(location);
+            }
+        }
 
         this.bowsLocation.put(location, armorStand);
     }
