5f352efee3116e4d1092a78c6580ed9383b7edb8
Red_Spash <red.spash.game@gmail.com>
2024-03-05 21:30:36 +0100

    Deleted ItemTrigger

--- src/fr/red_spash/murder/game/roles/ItemTrigger.java
+++
@@ -1,9 +0,0 @@
-package fr.red_spash.murder.game.roles;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
-public interface ItemTrigger {
-
-    void triggerAction(Player p, ItemStack itemStack);
-}
