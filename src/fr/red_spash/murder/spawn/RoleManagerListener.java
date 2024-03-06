4f258e42951bff591f17ef978cf33d3c605f94ea
Red_Spash <red.spash.game@gmail.com>
2024-03-06 19:38:02 +0100

    Added information in role menu

--- src/fr/red_spash/murder/spawn/RoleManagerListener.java
+++ src/fr/red_spash/murder/spawn/RoleManagerListener.java
@@ -24,11 +24,13 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class RoleManagerListener implements Listener {
 
     public static final String INVENTORY_NAME = "§d§lRôles de la partie";
     public static final String GAME_PRESET_COLOR = "§a§l";
+    public static final int SPACE_BEFORE_ROLES = 9;
     private final List<Role> allRoles;
     private final RoleConfiguration roleConfiguration;
     private final ArrayList<UUID> openedMenu;
@@ -66,10 +68,19 @@ public class RoleManagerListener implements Listener {
         );
         ArrayList<Role> allRolePreset = new ArrayList<>();
         for(Role role : this.allRoles){
-            allRolePreset.add(Utils.createNewInstance(role));
+            if(!role.getClass().equals(Innocent.class)){
+                allRolePreset.add(Utils.createNewInstance(role));
+            }
         }
-        this.gamePresets.add(new GamePreset("Tous les rôles",
-                allRolePreset)
+        GamePreset allRoleGamePreset = new GamePreset("Tous les rôles", allRolePreset);
+        this.gamePresets.add(allRoleGamePreset
+        );
+        this.roleConfiguration.loadPreset(allRoleGamePreset);
+
+        ArrayList<Role> roleWithoutSchizophrenic = new ArrayList<>();
+        roleWithoutSchizophrenic.addAll(allRolePreset.stream().filter(role -> !(role instanceof Schizophrenic)).collect(Collectors.toList()));
+        this.gamePresets.add(new GamePreset("Sans Schizophrène",
+                roleWithoutSchizophrenic)
         );
     }
 
@@ -104,27 +115,29 @@ public class RoleManagerListener implements Listener {
             this.openedMenu.add(p.getUniqueId());
         }
 
-        int sizeNeededForRoles = this.getNecessarySpaceFor(this.allRoles)*9;
-        int sizeNeededForPreset = this.getNecessarySpaceFor(this.gamePresets)*9;
+        int sizeNeededForRoles = this.getNecessarySpaceFor(this.allRoles)* SPACE_BEFORE_ROLES;
+        int sizeNeededForPreset = this.getNecessarySpaceFor(this.gamePresets)* SPACE_BEFORE_ROLES;
 
-
-        Inventory inventory = Bukkit.createInventory(null,sizeNeededForRoles+sizeNeededForPreset+9, INVENTORY_NAME+" §7§l("+this.roleConfiguration.getRoles().size()+"/"+Bukkit.getOnlinePlayers().size()+")");
+        Inventory inventory = Bukkit.createInventory(null,sizeNeededForRoles+sizeNeededForPreset+(SPACE_BEFORE_ROLES *3), INVENTORY_NAME);
         if(p.getOpenInventory().getTitle().startsWith(INVENTORY_NAME)){
             inventory = p.getOpenInventory().getTopInventory();
-            p.getOpenInventory().setTitle(INVENTORY_NAME+" §7§l("+this.roleConfiguration.getRoles().size()+"/"+Bukkit.getOnlinePlayers().size()+")");
         }
 
-        inventory.setItem(sizeNeededForRoles+8, new ItemStackBuilder(Material.LAPIS_LAZULI,Math.min(1,this.roleConfiguration.getRoles().size()))
-                .setName("§a"+this.roleConfiguration.getRoles()+" rôles attribué(s)")
+        inventory.setItem(sizeNeededForRoles+ SPACE_BEFORE_ROLES *2+2, new ItemStackBuilder(Material.LAPIS_LAZULI,Math.max(1,this.roleConfiguration.getRoles().size()))
+                .setName("§a"+this.roleConfiguration.getRoles().size()+" rôles attribué(s)")
                 .setLore("§7Permet de savoir le","§7nombre de rôle attribué","§7avec cette configuration")
                 .toItemStack());
-
-        inventory.setItem(sizeNeededForRoles+7, new ItemStackBuilder(Material.REDSTONE,Math.min(1,Bukkit.getOnlinePlayers().size()-this.roleConfiguration.getRoles().size()))
-                .setName("§a"+this.roleConfiguration.getRoles()+" rôles restants")
+        int remainingRoles = Math.max(0,Bukkit.getOnlinePlayers().size()-this.roleConfiguration.getRoles().size());
+        Material material = Material.REDSTONE;
+        if(remainingRoles == 0){
+            material = Material.BARRIER;
+        }
+        inventory.setItem(sizeNeededForRoles+ SPACE_BEFORE_ROLES *2+1, new ItemStackBuilder(material,Math.max(1,remainingRoles))
+                .setName("§a"+remainingRoles+" rôles restants")
                 .setLore("§7Permet de savoir le","§7nombre de rôle encore attribuable","§7avec cette configuration")
                 .toItemStack());
 
-        int index = 0;
+        int index = SPACE_BEFORE_ROLES;
         for(Role role : this.allRoles){
             int amount = this.roleConfiguration.getAmountRole(role);
             ArrayList<String> lore = new ArrayList<>(splitInSixWords(role.getDescription(),"§7"));
@@ -135,16 +148,26 @@ public class RoleManagerListener implements Listener {
             }
             lore.add("§f§7");
             lore.add("§fIl y a "+amount+" "+role.getName()+" dans la prochaine partie.");
+            String headURL = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvM2VkMWFiYTczZjYzOWY0YmM0MmJkNDgxOTZjNzE1MTk3YmUyNzEyYzNiOTYyYzk3ZWJmOWU5ZWQ4ZWZhMDI1In19fQ==";
+            if(amount >= 1){
+                headURL = role.getHeadUUID();
+            }
             ItemStack item = new ItemStackBuilder(Material.PLAYER_HEAD, Math.max(1,amount))
                     .setName(ChatColor.of(role.getRoleColor()) +"§l"+role.getName())
                     .setLore(lore)
-                    .setHeadTexture(role.getHeadUUID())
+                    .setHeadTexture(headURL)
+                    .hideAttributes()
                     .toItemStack();
+
+
             inventory.setItem(index, item);
             index = index + 1;
         }
-        index = inventory.getSize()-1;
+        index = inventory.getSize() - 1 - SPACE_BEFORE_ROLES - 4;
         for(GamePreset gamePreset : this.gamePresets){
+            if(index >= inventory.getSize()){
+                break;
+            }
             ArrayList<String> lore = new ArrayList<>();
             lore.add("§7Rôle du preset:");
             lore.addAll(gamePreset.getDescription());
@@ -155,16 +178,28 @@ public class RoleManagerListener implements Listener {
                             .setHeadTexture("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTJiMTcxMmI5MDdjZTZiMTQwMmVhYWMyOGVjMjRhNGQ5NTU2OGY0YWI4N2U1OTc5ODBjMTViMjJiYmJkN2E1In19fQ==")
                             .toItemStack()
             );
-            index = index - 1;
+            index = index + 1;
         }
+
+        material = Material.LIME_STAINED_GLASS_PANE;
+        if(Bukkit.getOnlinePlayers().size()-this.roleConfiguration.getRoles().size() < 0){
+            material = Material.RED_STAINED_GLASS_PANE;
+        }
+        ItemStack filledItem = new ItemStackBuilder(material).setName("§f").hideAttributes().toItemStack();
+        for(int i =0; i< inventory.getSize(); i++){
+            if(inventory.getItem(i) == null || inventory.getItem(i).getType().toString().contains("STAINED_GLASS_PANE")){
+                inventory.setItem(i,filledItem);
+            }
+        }
+
         if(!p.getOpenInventory().getTitle().equals(INVENTORY_NAME)){
             p.openInventory(inventory);
         }
     }
 
     private int getNecessarySpaceFor(List<?> list) {
-        int size = list.size()/9;
-        if(this.allRoles.size() % 9 != 0){
+        int size = list.size()/ 9;
+        if(list.size() % 9 != 0){
             size = size + 1;
         }
         return size;
@@ -199,11 +234,10 @@ public class RoleManagerListener implements Listener {
         ItemMeta itemMeta = itemStack.getItemMeta();
         if(!itemMeta.hasDisplayName())return;
         if(!p.getOpenInventory().getTitle().startsWith(INVENTORY_NAME))return;
+        e.setCancelled(true);
+        if(e.getRawSlot() - SPACE_BEFORE_ROLES < this.allRoles.size() && e.getRawSlot() - SPACE_BEFORE_ROLES >= 0){
+            Role clickedRole = this.allRoles.get(e.getRawSlot() - SPACE_BEFORE_ROLES);
 
-        if(e.getSlot() < this.allRoles.size()){
-            Role clickedRole = this.allRoles.get(e.getSlot());
-
-            e.setCancelled(true);
             if(e.getAction() == InventoryAction.PICKUP_ALL){
                 this.roleConfiguration.addRole(Utils.createNewInstance(clickedRole));
                 p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT,1,2);
@@ -233,6 +267,7 @@ public class RoleManagerListener implements Listener {
                 if(displayName.equals(GAME_PRESET_COLOR+gamePreset.getName())){
                     this.roleConfiguration.loadPreset(gamePreset);
                     Bukkit.broadcastMessage("§bLe preset §l"+gamePreset.getName()+"§r§b vient d'être chargé!");
+                    p.playSound(p.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL,1,1);
                     this.updateInventories();
                     return;
                 }
