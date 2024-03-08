e50e363bd726fa4d5aad1462e76208aebe4a607a
Red_Spash <red.spash.game@gmail.com>
2024-03-08 15:09:58 +0100

    Added time between each message

--- src/fr/red_spash/murder/event/ChatListener.java
+++ src/fr/red_spash/murder/event/ChatListener.java
@@ -5,14 +5,26 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class ChatListener implements Listener {
 
+    private final HashMap<UUID, Long> chatCooldown = new HashMap<>();
 
     @EventHandler
     public void asyncPlayerChatEvent(AsyncPlayerChatEvent e){
         Player p = e.getPlayer();
         if (p.isOp()){
             e.setMessage(e.getMessage().replace("&","§"));
+        }else{
+            if(this.chatCooldown.containsKey(p.getUniqueId()) &&
+                    (this.chatCooldown.get(p.getUniqueId()) > System.currentTimeMillis())){
+                e.setCancelled(true);
+                p.sendMessage("§cVous devez attendre avant d'envoyer un nouveau message!");
+                return;
+            }
+            this.chatCooldown.put(p.getUniqueId(),System.currentTimeMillis()+1000);
         }
         e.setFormat(getPrefix(p)+p.getName()+" §7>>>§f "+e.getMessage());
     }
