e5b9e9b876c32112bf32f458fb976c968ee87488
Red_Spash <red.spash.game@gmail.com>
2024-09-06 15:16:49 +0200

    fix: correct chat format

--- src/fr/red_spash/murder/event/ChatListener.java
+++ src/fr/red_spash/murder/event/ChatListener.java
@@ -1,5 +1,11 @@
 package fr.red_spash.murder.event;
 
+import fr.red_spash.murder.game.GameManager;
+import fr.red_spash.murder.game.GameState;
+import fr.red_spash.murder.players.PlayerData;
+import fr.red_spash.murder.players.PlayerManager;
+import fr.red_spash.murder.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +17,13 @@ import java.util.UUID;
 public class ChatListener implements Listener {
 
     private final HashMap<UUID, Long> chatCooldown = new HashMap<>();
+    private final PlayerManager playerManager;
+    private final GameManager gameManager;
+
+    public ChatListener(PlayerManager playerManager, GameManager gameManager) {
+        this.playerManager = playerManager;
+        this.gameManager = gameManager;
+    }
 
     @EventHandler
     public void asyncPlayerChatEvent(AsyncPlayerChatEvent e){
@@ -26,17 +39,38 @@ public class ChatListener implements Listener {
             }
             this.chatCooldown.put(p.getUniqueId(),System.currentTimeMillis()+1000);
         }
-        e.setFormat(getPrefix(p)+p.getName()+" §7>>>§f "+e.getMessage());
+        PlayerData playerData = this.playerManager.getData(p.getUniqueId());
+        String message = getPrefix(p)+p.getName()+" §7>>>§7 "+e.getMessage();
+        e.setCancelled(true);
+        if(playerData.isSpectator()){
+            message = "§7[SPECTATEUR] "+message;
+            for(Player pl : Bukkit.getOnlinePlayers()){
+                PlayerData data = this.playerManager.getData(pl);
+                if(data.isSpectator()){
+                    pl.sendMessage(message);
+                }
+            }
+        }else{
+            if(this.gameManager.getGameState() == GameState.IN_GAME){
+                for(Player pl : Bukkit.getOnlinePlayers()){
+                    double distance = Utils.round(pl.getLocation().distance(p.getLocation()),2);
+                    if(distance <= 20){
+                        pl.sendMessage("§7["+ distance+"m]"+message);
+                    }
+                }
+            }else{
+                Bukkit.broadcastMessage(message);
+            }
+        }
     }
 
     public static String getPrefix(Player p){
         if(p.getName().equals("Red_Spash")){
             return "§c[Développeur] ";
-        }else{
+        } else if (p.getName().equals("Abidex_")) {
+            return "§c[Admin] ";
+        } else{
             return "§6";
         }
     }
-
-
-
 }
